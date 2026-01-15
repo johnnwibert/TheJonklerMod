ca9745d9fb5a2a4604fff0ee4b92cf4c6050b886
John Noel Wibert <johnnoelwibert@gmail.com>
2026-01-15 15:55:31 -0500

    Decay Spectral added, comments added

--- src/spectrals.lua
+++ src/spectrals.lua
@@ -8,6 +8,10 @@ SMODS.Atlas {
 loc_colour()
 G.ARGS.LOC_COLOURS.jonkler_navy = HEX("00007a")
 
+-- Offering
+-- Adds a Navy Seal to up to 2 selected cards.
+-- Sort of creates a sustaining loop of spectrals as long
+-- as you can keep making more of these cards.
 SMODS.Spectral {
     key = 'offering',
     discovered = true,
@@ -57,6 +61,10 @@ SMODS.Spectral {
     end,
 }
 
+-- Enchant
+-- Turns a selected card into a Geel Card.
+-- Typically, enhancements are given by tarots. Because this enhancement
+-- is very powerful, it seemed more balanced for a spectral to give it instead.
 SMODS.Spectral {
     key = "enchant",
     discovered = true,
@@ -134,5 +142,57 @@ SMODS.Spectral {
     end,
     can_use = function(self, card)
         return G.hand and #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.max_highlighted
+    end
+}
+
+-- Decay
+-- Gives +1 hand size, but sets all non-eternal jokers perishable.
+-- Hand size is very valuable, especially early, but it can be
+-- devastating to lose your build if you get this at the wrong time.
+SMODS.Spectral {    -- Todo: Check jokers that can't be perishable and add hand size
+    key = 'decay',
+    discovered = true,
+    pos = { x = 2, y = 0 }, -- No Art Yet
+    atlas = "jonkler_spectrals",
+    loc_txt = {
+        label = "Decay",
+        name = "Decay",
+        text = {
+            "Gives {C:attention}+#1#{} hand size, sets all",
+            "non-Eternal {C:attention}Jokers{} Perishable",
+        }
+    },
+    config = { extra = { hand_size_mod = 1 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.hand_size_mod } }
+    end,
+    use = function(self, card, area, copier)
+        local jokers_to_decay = {}
+        for k, v in pairs(G.jokers.cards) do
+            if not v.ability.eternal and not v.ability.perishable then
+                table.insert(jokers_to_decay, v)
+            end
+        end
+        if jokers_to_decay then
+            for i = 1, #jokers_to_decay do
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound('tarot1')
+                        jokers_to_decay[i]:juice_up(0.8, 0.8)
+                        jokers_to_decay[i].ability.perishable = true
+                        return true
+                    end
+                }))
+                delay(0.2)
+            end
+        end
+    end,
+    can_use = function(self, card)
+        for k, v in pairs(G.jokers.cards) do
+            if not v.ability.eternal and not v.ability.perishable then
+                return true
+            end
+        end
+        return false
     end
 }
