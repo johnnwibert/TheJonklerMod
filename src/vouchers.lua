d638193a0158a07f07c237515ddc8e695eeb727f
John Noel Wibert <johnnoelwibert@gmail.com>
2026-01-23 18:40:26 -0500

    Amplify progress, stone upgrade working(?)

--- src/vouchers.lua
+++ src/vouchers.lua
@@ -8,12 +8,12 @@ SMODS.Atlas {
 -- Amplify Voucher
 -- Makes Enhancements stronger (Do Purify next)
 SMODS.Voucher {
-    key = "amplify",
+    key = "amplify",        -- xchips in hand is working, but even when voucher isn't purchased (fix)
     pos = { x = 0, y = 0 },
     discovered = true,
-    config = { extra = { h_Xchips = 1.5 } },
+    config = { h_x_chips = 1.5 },
     loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.h_Xchips } }
+        return { vars = { card.ability.h_x_chips } }
     end,
     loc_txt = {
         name = "Amplify",
@@ -22,15 +22,10 @@ SMODS.Voucher {
         }
     },
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.hand and not context.end_of_round then
-            if SMODS.has_enhancement(context.other_card, 'm_stone') then
-                print("m_stone detected")   -- This worked, look into why x chips isn't working
-            end
-            if SMODS.has_enhancement(context.other_card, 'm_jonkler_stone_upgrade') then
-                print("m_jonkler_stone_upgrade detected")
-            end
+        if context.individual and context.cardarea == G.hand and not context.end_of_round and SMODS.has_enhancement(context.other_card, 'm_stone') then
+            print("m_stone detected")
             return {
-                h_x_chips = card.ability.extra.h_Xchips,
+                xchips = card.ability.h_x_chips,
                 message_card = context.other_card
             }
         end
@@ -42,13 +37,13 @@ SMODS.Enhancement:take_ownership('stone', {
     no_rank = true,
     no_suit = true,
     always_scores = true,
-    config = { bonus = 50, h_Xchips = 1.5 },
+    config = { bonus = 50, extra = { h_Xchips = 1.5 } },
     loc_vars = function(self, info_queue, card)
         local key = 'm_stone'
         if G.GAME.used_vouchers['v_jonkler_amplify'] then
             key = 'm_jonkler_stone_upgrade'
         end
-        return { key = key, vars = { card.ability.bonus, card.ability.h_Xchips } }
+        return { key = key, vars = { card.ability.bonus, card.ability.extra.h_Xchips } }
     end,
 }
 )
